b3f329402d6137c830d18f403c37478fd4b4d26a
Mauro Bono <bono.mauro@gmail.com>
2019-09-26 15:31:49 +0200

    contact page and netlify form

---
+++ src/components/styled/StyledForm.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+const StyledFormWrapper = styled.div`
+  max-width: 500px;
+  margin: 3rem auto;
+`;
+
+const StyledH2 = styled.h2`
+  text-align: center;
+  font-family: "Nunito", sans-serif;
+  font-weight: 400;
+  font-size: 2.4rem;
+  color: var(--primary-color);
+  text-transform: uppercase;
+`;
+
+const StyledForm = styled.form`
+  max-width: 400px;
+  margin: 3rem auto;
+  padding: 2rem;
+  font-family: "Nunito", sans-serif;
+  font-size: 1.6rem;
+  color: var(--paragraph-color);
+  box-shadow: 0px 70px 40px -50px rgba(0, 0, 0, 0.2),
+    0px 3px 20px 8px rgba(0, 0, 0, 0.1);
+
+  label {
+    display: block;
+    padding: 1rem 0 0.5rem;
+  }
+
+  input,
+  textarea {
+    display: block;
+    width: 100%;
+    border: 1px solid var(--meta-color);
+    padding: 0.5rem;
+    border-radius: 5px;
+    font-family: "Nunito", sans-serif;
+    font-size: 1.6rem;
+    color: var(--paragraph-color);
+  }
+
+  #sendBtn {
+    border: 0;
+    background: var(--primary-color);
+    padding: 1rem 0.5rem;
+    color: #fff;
+    margin: 2rem 0;
+    width: 100%;
+    text-transform: uppercase;
+    transition: all 0.35ms ease-in-out;
+    cursor: pointer;
+
+    &:hover {
+      background: #3498db;
+    }
+  }
+`;
+
+export { StyledFormWrapper, StyledH2, StyledForm };
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,22 +3,55 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Paragraph from "../components/styled/Paragraph";
 import PageTitle from "../components/styled/PageTitle";
+import { useSpring, animated, config } from "react-spring";
+import {
+  StyledFormWrapper,
+  StyledForm,
+  StyledH2,
+} from "../components/styled/StyledForm";
 
-const Contact = () => (
-  <Layout>
-    <SEO title="Contact" />
-    <PageTitle>Contact Me</PageTitle>
-    <Paragraph>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, ipsum
-      architecto sunt beatae quam deserunt laboriosam? Corporis eaque est sint
-      ipsa commodi dolor minus ducimus praesentium magnam nemo ab optio iste,
-      nostrum qui asperiores odio! Totam rem assumenda aliquam maiores ea iste
-      esse, consectetur voluptatibus! Sunt recusandae nulla deserunt blanditiis
-      voluptates reprehenderit sapiente ex fugiat aliquid placeat soluta neque,
-      numquam possimus illo in ab sequi. Fugiat magnam non voluptatibus possimus
-      fugit tenetur.
-    </Paragraph>
-  </Layout>
-);
+const Contact = () => {
+  const hint = "Let's Build \nSomething \nGreat !";
+
+  const slideIn = useSpring({
+    config: config.slow,
+    from: { opacity: 0, marginLeft: -500 },
+    to: { opacity: 1, marginLeft: 0 },
+  });
+
+  const fade = useSpring({
+    config: { duration: 2000 },
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  });
+
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <animated.div style={slideIn}>
+        <PageTitle>Contact Me</PageTitle>
+      </animated.div>
+      <StyledFormWrapper>
+        <animated.div style={fade}>
+          <StyledForm name="Contact Form" method="POST" data-netlify="true">
+            <label for="name">Name</label>
+            <input type="text" id="name" />
+            <label for="email">Email</label>
+            <input type="text" id="email" />
+            <label for="message">Message</label>
+            <textarea id="message" cols="30" rows="8"></textarea>
+            <input type="button" value="Send" id="sendBtn" />
+            <div data-netlify-recaptcha="true"></div>
+          </StyledForm>
+          <StyledH2>{hint}</StyledH2>
+          <Paragraph>
+            If you like my works and have some cool project to build, just fill
+            the form or contact me through my social media listed below.
+          </Paragraph>
+        </animated.div>
+      </StyledFormWrapper>
+    </Layout>
+  );
+};
 
 export default Contact;
